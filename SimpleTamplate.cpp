5d032855362713529b878b978cb62b84a26b0c08
Saifur <88000678+BrownFalcon001@users.noreply.github.com>
2022-08-03 02:11:40 +0600

    Create SimpleTamplate.cpp

---
+++ SimpleTamplate.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define INFI 1e18
+#define pii pair<ll, ll>
+
+int main() {
+  ios_base:: sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+
+
+  return 0;
+}
