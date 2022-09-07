5a99aa84b4c2e7108b5a173521d0d9289190efc1
Saifur <88000678+BrownFalcon001@users.noreply.github.com>
2022-09-07 20:56:41 +0600

    Update SimpleTamplate.cpp

--- SimpleTamplate.cpp
+++ SimpleTamplate.cpp
@@ -7,11 +7,13 @@ using namespace std;
 #define endl "\n"
 
 int main() {
-  ios_base:: sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base:: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
 
 
-  return 0;
+
+
+    return 0;
 }
