cb31ea893b13647ce2ba467f40e4a179b110f506
Saifur <88000678+BrownFalcon001@users.noreply.github.com>
2022-09-07 21:14:52 +0600

    Update SimpleTamplate.cpp

--- SimpleTamplate.cpp
+++ SimpleTamplate.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #define pii pair<ll, ll>
 #define piii pair<ll, pii>
 #define endl "\n"
+#define TC ll t;cin>>t;while(t--)
 
 int main() {
     ios_base:: sync_with_stdio(false);
