2f0e0b2d8699dd19f1e842e942bd9be98aa12ead
Saifur <88000678+BrownFalcon001@users.noreply.github.com>
2022-09-22 12:30:55 +0600

    Update SimpleTamplate.cpp

--- SimpleTamplate.cpp
+++ SimpleTamplate.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define INFI 1e18
 #define pii pair<ll, ll>
 #define piii pair<ll, pii>
 #define endl "\n"
 #define TC ll t;cin>>t;while(t--)
+const ll INFI = 1e18;
 const ll MOD =  1e9+7;
 
 int main() {
