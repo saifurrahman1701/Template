41ef13b90a5b35d6a09c413bfb0ab7ccdf3e1276
Saifur <88000678+BrownFalcon001@users.noreply.github.com>
2023-03-05 23:46:36 +0600

    Update SimpleTamplate.cpp

--- SimpleTamplate.cpp
+++ SimpleTamplate.cpp
@@ -1,3 +1,6 @@
+/*
+ *   author: brownFalcon
+ */
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -6,17 +9,30 @@ using namespace std;
 #define endl "\n"
 #define TC ll t;cin>>t;while(t--)
 #define CHECK_BIT(var,pos) (((var)>>(pos)) & 1)
+
+#define read freopen("input.txt", "r", stdin)
+#define write freopen("output.txt", "w", stdout)
+
+#define fastio()                        \
+    ios_base::sync_with_stdio(false);   \
+    cin.tie(NULL);                      \
+    cout.tie(0)
+
 const ll INFI = 1e18;
 const ll MOD =  1e9+7;
 
+
+
 int main() {
-	ios_base:: sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	read;
+	write;
+#else
+	fastio();
+#endif
+	
 
-
-
-
+	
 
 	return 0;
 }
